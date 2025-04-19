4e51b3d8dd87de79141cc161a79cba11326d8d7a
tsavage6001 <130178364+tsavage6001@users.noreply.github.com>
2025-04-19 16:57:12 -0400

    Update __init__.py

--- grpo_trs2/verifiers/envs/__init__.py
+++ grpo_trs2/verifiers/envs/__init__.py
@@ -4,6 +4,6 @@ from .multiturn_env import MultiTurnEnv
 
 #from .doublecheck_env import DoubleCheckEnv
 from .code_env import CodeEnv
-from .tool_env import ToolEnv
+#from .tool_env import ToolEnv
 
 __all__ = ['Environment', 'SimpleEnv', 'MultiTurnEnv', 'DoubleCheckEnv', 'CodeEnv', 'ToolEnv']
